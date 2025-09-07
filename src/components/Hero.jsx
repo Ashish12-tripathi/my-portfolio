ecc630b7bb96a24b69ea615a0cb33a2d47724277
Ashish tripathi <156857802+Ashish12-tripathi@users.noreply.github.com>
2025-09-07 14:59:43 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,8 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Ashish</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am full stack developer, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I am full stack developer, <br className='sm:block hidden' />
+            Turning Ideas into Code
           </p>
         </div>
       </div>
